2e5ff079992ac0c06f911d95668f28dfbc80658d
Stelvio-Chibuco <stelviochibuco@outlook.com>
2024-03-25 16:16:43 +0200

    Actualização

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,12 +10,12 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by DeveloperFolio")}
+          {emoji("Made with ❤️  by DeveloperFolio")}
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
           <a href="https://github.com/Stelvio-Chibuco">
-            
+            cybergorilla
           </a>
         </p>
       </div>
